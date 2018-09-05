9fa64dc0a385c8a2ca7ae87546d2add0a765a565
Rajesh bezavada <41729529+Rajeshbezavada@users.noreply.github.com>
2018-09-05 00:43:47 -0400

    Create Kelvin.java

---
+++ Kelvin.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+class Kelvin
+{
+public static void main(String aa[]){
+    int a,b,c;
+    Scanner s=new Scanner(System.in);
+    a=s.nextInt();
+    int n=a+273;
+    System.out.println(n);
+
+           
+   }
+}
